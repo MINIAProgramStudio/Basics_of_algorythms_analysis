7ad2c333793ee8d2544f0e53dfe6c7f4dd56b75d
Mykhailo Holub <mishataube2005@gmail.com>
2024-11-11 08:11:05 +0200

    aaaaaaaaaaaaaaa

--- Parser.py
+++ Parser.py
@@ -18,7 +18,7 @@ OBLIGATORY_KEYWORD
 """
 
 commands = {
-    "CREATE": "CREATE table_name (1 , 2)",
+    "CREATE": "CREATE table_name (1, 2)",
     "INSERT": "INSERT {INTO} table_name (1)",
     "SELECT": "SELECT [(1)] FROM table_name [WHERE condition] [GROUP_BY (1)}"
 }
@@ -48,3 +48,61 @@ def prepare_input(string):
             break
         i+=1
     return string
+
+def parse(prepared_input, commands_dict):
+    if prepared_input[0] in commands_dict.keys():
+        args = {}
+        command = commands_dict[prepared_input[0]]
+        p_counter = 0
+        c_counter = 0
+        while p_counter < len(prepared_input):
+            temp = ""
+            while c_counter<len(command):
+                match command[c_counter]:
+                    case " ":
+                        break
+                    case _:
+                        temp += command[c_counter]
+                        c_counter += 1
+            if temp.upper() == temp:
+                if prepared_input[p_counter] == temp:
+                    p_counter += 1
+                    c_counter += 1
+                    continue
+                elif temp[0] == "{" and temp[-1] == "}":
+                    if prepared_input[p_counter] == temp[1:-1]:
+                        args[temp[1:-1]] = True
+                    else:
+                        args[temp[1:-1]] = False
+                elif temp[0] == "[" or temp[-1] in [")","]","}"]:
+                    pass
+                else:
+                    return -2
+            if temp.lower() == temp:
+                args[temp] = prepared_input[p_counter]
+                p_counter += 1
+                c_counter += 1
+                continue
+
+            if temp[0] == "(" and temp[-1] == ")":
+                if len(temp) > 2:
+                    lengths = []
+                    br_temp = ""
+                    for symbol in temp:
+                        match symbol:
+                            case "(":
+                                continue
+                            case ",":
+                                lengths.append(int(br_temp))
+                            case " ":
+                                continue
+                            case ")":
+                                lengths.append(int(br_temp))
+                            case _:
+                                br_temp+=symbol
+
+
+
+
+    else:
+        return -1
