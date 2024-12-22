e322f353bd4aa97fa2f0700a701cf3a248536490
Mykhailo Holub <mishataube2005@gmail.com>
2024-12-22 16:36:27 +0200

    Create Data_structure.py

---
+++ Data_structure.py
@@ -0,0 +1,21 @@
+class Table:
+    def __init__(self, column_names):
+        self.column_names = column_names
+        self.rows = []
+    def __str__(self):
+        output = ["\n"+str(row) for row in self.rows]
+
+class Data:
+    def __init__(self):
+        self.tables = {}
+
+    def create(self, table_name, column_names):
+        if table_name in self.tables.keys():
+            return -2 # table already exsists
+        self.tables[table_name] = Table(column_names)
+
+    def __str__(self):
+        output = "There are "+len(self.tables)" tables:"
+        for key in self.tables.keys():
+            output += "\n"+key
+        return output
