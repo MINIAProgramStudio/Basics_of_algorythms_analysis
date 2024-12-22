9e28905e3a4d3655e44586b89b83e25289e819f0
Mykhailo Holub <mishataube2005@gmail.com>
2024-12-22 16:36:51 +0200

    error handling implemented

--- main.py
+++ main.py
@@ -1,2 +1,33 @@
 import Parser
-print("<<<",Parser.parse(Parser.prepare_input(Parser.recive_input()), Parser.commands))
+import Data_structure
+
+Data = Data_structure.Data()
+
+while True:
+    print()
+    string = input("<<<")
+    parsing_result = Parser.parse(Parser.prepare_input(Parser.recive_input()), Parser.commands)
+    if isinstance(parsing_result, list):
+        if isinstance(parsing_result[1], list):
+            pass
+        else:
+            match parsing_result[0]:
+                case "CREATE":
+                    match parsing_result[1]:
+                        case -2:
+                            print(">>>INVALID INPUT. No spaces encountered. Must be at least two spaces in CREATE.")
+                        case -3.1:
+                            print(">>>INVALID INPUT. No opening bracket found.")
+                        case -3.2:
+                            print(">>>INVALID INPUT. No closing bracket found.")
+                        case -4:
+                            print(">>>INVALID INPUT. Wrong comma placement")
+                        case -5:
+                            print(">>>INVALID INPUT. Invalid character.")
+
+    else:
+        match parsing_result:
+            case -3:
+                print(">>>INVALID INPUT. No spaces encountered. Must be at least one space.")
+            case -2:
+                print(">>>FAILED TO PARSE. No such command.")
