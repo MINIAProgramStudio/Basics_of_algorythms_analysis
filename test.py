f99bd8147047f86567294eaedb09d13c28729286
Mykhailo Holub <mishataube2005@gmail.com>
2024-12-23 12:05:31 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,46 +1,78 @@
 from Data_structure import *
-from time import time
+from time import time, sleep
 from random import random
 import PythonTableConsole as PTC
+import tqdm
 
 Database = Data()
+ERROR = False
 
 
-number_of_random_values = [10**i for i in range(1,7)] # 8 == 4GB RAM, 9 == 12GB RAM, 10 is not recommended
-measurements = [["number of values", "time to create plain", "time to create indexed",
+number_of_random_values = [10**i for i in range(1,6)]
+measurements = [["number of values", "time to insert into plain", "time to insert into indexed",
                 "time to find equals plain", "time to find equals indexed"]]
 values = []
 for n in number_of_random_values:
+    sleep(0.1)
+    print("Iteration "+str(n))
     Database.create("plain"+str(n), ["height", "weight", "age"], False)
     Database.create("index"+str(n), ["height", "weight", "age"], [True, True, True])
     measurements.append([n,0,0,0,0])
 
-    for i in range(n-len(values)):
+    for i in tqdm.tqdm(range(n-len(values)),desc="Generating new values"):
         values.append([int(140+random()*70), int(50+random()*100), int(random()*100)])
 
-
-    # create plain
+    sleep(0.1)
+    # insert plain
     measurements[-1][1] = -time()
-    for value in values:
-        Database.insert("plain"+str(n), value)
+    for value in tqdm.tqdm(values, desc="Inserting plain " + str(n)):
+        temp = Database.insert("plain"+str(n), value)
+        if isinstance(temp,int):
+            if temp<0:
+                print(temp)
+                print(value)
+                exit()
     measurements[-1][1] += time()
 
-    # create indexed
+    sleep(0.1)
+    # insert indexed
     measurements[-1][2] = -time()
-    for value in values:
-        Database.insert("index"+str(n), value)
+    for value in tqdm.tqdm(values,desc="Inserting indexed " + str(n)):
+        temp = Database.insert("index" + str(n), value)
+        if isinstance(temp, int):
+            if temp < 0:
+
+                print(temp)
+                print(value)
+                exit()
     measurements[-1][2] += time()
 
-    # find equal plain
-    measurements[-1][3] = -time()
-    Database.select("plain"+str(n),False,["height",values[0][0]])
-    measurements[-1][3] += time()
-
+    sleep(0.1)
     # find equal indexed
     measurements[-1][4] = -time()
-    Database.select("index" + str(n), False, ["height", values[0][0]])
+    for i in tqdm.tqdm(range(n), desc="Finding equal indexed " + str(n)):
+        temp = Database.select("index" + str(n), False, ["height", values[i][0]])
+        if isinstance(temp, int):
+            if temp < 0:
+                print(temp)
+                exit()
     measurements[-1][4] += time()
 
-table = PTC.PythonTableConsole(measurements)
-table.transpose()
-print(table)
+    sleep(0.1)
+    # find equal plain
+    measurements[-1][3] = -time()
+    for i in tqdm.tqdm(range(n),desc = "Finding equal plain " + str(n)):
+        temp = Database.select("plain"+str(n),False,["height",values[i][0]])
+        if isinstance(temp, int):
+            if temp < 0:
+                print(temp)
+                exit()
+    measurements[-1][3] += time()
+
+
+
+    table = PTC.PythonTableConsole(measurements)
+    table.transpose()
+    print(table)
+
+input("Press enter to exit")
